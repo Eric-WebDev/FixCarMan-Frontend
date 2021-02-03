4a5f5c84bdd8438cb1f4c2f737153bd56177f1dc
arek512 <A_lesica@outlook.com>
2021-02-03 12:46:21 +0000

    Add some style

--- src/features/nav/NavBar.tsx
+++ src/features/nav/NavBar.tsx
@@ -12,7 +12,7 @@ import { Button, Container, Menu, MenuItem, Segment } from "semantic-ui-react";
           FixCarMan
         </Menu.Item>
 
-        <Menu.Item name="home" as={NavLink} to='/'/>
+        <Menu.Item name="home" as={NavLink} exact to='/'/>
         <Menu.Item name="garages" as={NavLink} to='/garages'/>
         <Menu.Item name="Search Ad" as={NavLink} to='/adverts'/>
         <Menu.Item>
