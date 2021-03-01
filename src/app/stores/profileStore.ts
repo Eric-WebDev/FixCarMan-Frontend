4ed91fbb5d66e3e4972f55bda10d440f0c20cdce
arek512 <A_lesica@outlook.com>
2021-03-01 17:09:32 +0000

    Add profile store

---
+++ src/app/stores/profileStore.ts
@@ -0,0 +1,63 @@
+import { RootStore } from './rootStore';
+import { observable, action, runInAction, computed, reaction } from 'mobx';
+import agent from '../api/agent';
+import { toast } from 'react-toastify';
+import { IProfile } from '../models/users/user';
+
+export default class ProfileStore {
+  rootStore: RootStore;
+  constructor(rootStore: RootStore) {
+    this.rootStore = rootStore;
+  }
+
+  @observable profile: IProfile | null = null;
+  @observable loadingProfile = true;
+  @observable loading = false;
+  @observable activeTab: number = 0;
+
+
+  @computed get isCurrentUser() {
+    if (this.rootStore.userStore.user && this.profile) {
+      return this.rootStore.userStore.user.username === this.profile.username;
+    } else {
+      return false;
+    }
+  }
+
+  @action setActiveTab = (activeIndex: number) => {
+      this.activeTab = activeIndex;
+  } 
+
+  @action loadProfile = async (username: string) => {
+    this.loadingProfile = true;
+    try {
+      const profile = await agent.Profiles.get(username);
+      runInAction(() => {
+        this.profile = profile;
+        this.loadingProfile = false;
+      });
+    } catch (error) {
+      runInAction(() => {
+        this.loadingProfile = false;
+      });
+      console.log(error);
+    }
+  };
+
+
+  @action updateProfile = async (profile: Partial<IProfile>) => {
+    try {
+      await agent.Profiles.updateProfile(profile);
+      runInAction(() => {
+        if (
+          profile.username !== this.rootStore.userStore.user!.username
+        ) {
+          this.rootStore.userStore.user!.username = profile.username!;
+        }
+        this.profile = { ...this.profile!, ...profile };
+      });
+    } catch (error) {
+      toast.error('Problem updating profile');
+    }
+  };
+}
---
+++ src/features/profiles/ProfilePage.tsx
@@ -0,0 +1,46 @@
+import React, { useContext, useEffect } from 'react';
+import { Grid } from 'semantic-ui-react';
+import { RouteComponentProps } from 'react-router';
+import { RootStoreContext } from '../../app/stores/rootStore';
+import { observer } from 'mobx-react-lite';
+
+interface RouteParams {
+  username: string;
+}
+
+interface IProps extends RouteComponentProps<RouteParams> {}
+
+const ProfilePage: React.FC<IProps> = ({ match }) => {
+  const rootStore = useContext(RootStoreContext);
+  const {
+    loadingProfile,
+    profile,
+    loadProfile,
+    isCurrentUser,
+    loading,
+    setActiveTab
+  } = rootStore.profileStore;
+
+  useEffect(() => {
+    loadProfile(match.params.username);
+  }, [loadProfile, match]);
+
+  if (loadingProfile) return <LoadingComponent content='Loading profile...' />;
+
+  return (
+    <Grid>
+      <Grid.Column width={16}>
+        <ProfileHeader
+          profile={profile!}
+          isCurrentUser={isCurrentUser}
+          loading={loading}
+          follow={follow}
+          unfollow={unfollow}
+        />
+        <ProfileContent setActiveTab={setActiveTab} />
+      </Grid.Column>
+    </Grid>
+  );
+};
+
+export default observer(ProfilePage);
