cc492540e082f389515ca64dab9fa3f63e00a8d2
arek512 <A_lesica@outlook.com>
2021-03-09 16:01:19 +0000

    Change structure

--- src/app/stores/profileStore.ts
+++ src/app/stores/profileStore.ts
@@ -2,7 +2,7 @@ import { RootStore } from './rootStore';
 import { observable, action, runInAction, computed, reaction } from 'mobx';
 import agent from '../api/agent';
 import { toast } from 'react-toastify';
-import { IProfile } from '../models/users/user';
+import { IPhoto, IProfile, IUserAdvert } from '../models/profiles/profile';
 
 export default class ProfileStore {
   rootStore: RootStore;
@@ -12,9 +12,11 @@ export default class ProfileStore {
 
   @observable profile: IProfile | null = null;
   @observable loadingProfile = true;
+  @observable uploadingPhoto = false;
   @observable loading = false;
   @observable activeTab: number = 0;
-
+  @observable userAdverts: IUserAdvert[] = [];
+  @observable loadingAdverts = false;
 
   @computed get isCurrentUser() {
     if (this.rootStore.userStore.user && this.profile) {
@@ -23,7 +25,21 @@ export default class ProfileStore {
       return false;
     }
   }
-
+  @action loadUserAdverts = async (username: string, predicate?: string) => {
+    this.loadingAdverts = true;
+    try {
+      const adverts = await agent.Profiles.listAdverts(username, predicate!);
+      runInAction(() => {
+        this.userAdverts = adverts;
+        this.loadingAdverts = false;
+      })
+    } catch (error) {
+      toast.error('Problem loading activities')
+      runInAction(() => {
+        this.loadingAdverts = false;
+      })
+    }
+  }
   @action setActiveTab = (activeIndex: number) => {
       this.activeTab = activeIndex;
   } 
@@ -74,4 +90,46 @@ export default class ProfileStore {
       toast.error('Problem updating profile');
     }
   };
+
+  @action uploadPhoto = async (file: Blob) => {
+    this.uploadingPhoto = true;
+    try {
+      const photo = await agent.Profiles.uploadPhoto(file);
+      runInAction(() => {
+        if (this.profile) {
+          this.profile.photos.push(photo);
+          if (photo.isMain && this.rootStore.userStore.user) {
+            this.rootStore.userStore.user.image = photo.url;
+            this.profile.image = photo.url;
+          }
+        }
+        this.uploadingPhoto = false;
+      });
+    } catch (error) {
+      console.log(error);
+      toast.error('Problem uploading photo');
+      runInAction(() => {
+        this.uploadingPhoto = false;
+      });
+    }
+  };
+  
+  @action deletePhoto = async (photo: IPhoto) => {
+    this.loading = true;
+    try {
+      await agent.Profiles.deletePhoto(photo.id);
+      runInAction(() => {
+        this.profile!.photos = this.profile!.photos.filter(
+          a => a.id !== photo.id
+        );
+        this.loading = false;
+      });
+    } catch (error) {
+      toast.error('Problem deleting the photo');
+      runInAction(() => {
+        this.loading = false;
+      });
+    }
+  };
+
 }
